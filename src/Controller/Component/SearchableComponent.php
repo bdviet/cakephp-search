f3230edec76d3f853d0fe89361966669e2403159
Antonis Flangofas <a.flangofas@qobo.biz>
2016-03-28 15:48:34 +0300

    Baked component (task #1615)

---
+++ src/Controller/Component/SearchableComponent.php
@@ -0,0 +1,19 @@
+<?php
+namespace Search\Controller\Component;
+
+use Cake\Controller\Component;
+use Cake\Controller\ComponentRegistry;
+
+/**
+ * Searchable component
+ */
+class SearchableComponent extends Component
+{
+
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [];
+}
