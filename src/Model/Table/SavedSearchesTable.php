181f124ba5ca348f025fddf0b5da7436db43b188
George Constantinou <g.constantinou@qobo.biz>
2016-04-06 11:05:45 +0300

    saved search table class (task #1596)

---
+++ src/Model/Table/SavedSearchesTable.php
@@ -0,0 +1,86 @@
+<?php
+namespace Search\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+use Search\Model\Entity\SavedSearch;
+
+/**
+ * SavedSearches Model
+ *
+ * @property \Cake\ORM\Association\BelongsTo $Users
+ */
+class SavedSearchesTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('saved_searches');
+        $this->displayField('name');
+        $this->primaryKey('id');
+
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER',
+            'className' => 'Search.Users'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->uuid('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name', 'update')
+            ->allowEmpty('name', 'create');
+
+        $validator
+            ->requirePresence('type', 'create')
+            ->notEmpty('type');
+
+        $validator
+            ->requirePresence('model', 'create')
+            ->notEmpty('model');
+
+        $validator
+            ->requirePresence('shared', 'create')
+            ->notEmpty('shared');
+
+        $validator
+            ->requirePresence('content', 'create')
+            ->notEmpty('content');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['user_id'], 'Users'));
+        return $rules;
+    }
+}
