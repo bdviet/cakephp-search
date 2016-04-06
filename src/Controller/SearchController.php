2cbebf6b57f74d0583173572815264d23b81bd77
George Constantinou <g.constantinou@qobo.biz>
2016-04-06 11:58:15 +0300

    save search criteria or results (task #1596)

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -32,6 +32,27 @@ class SearchController extends AppController
     }
 
     /**
+     * Save action
+     *
+     * @param string|null $id Search id.
+     * @return void Redirects to advanced action.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function save($id = null)
+    {
+        $this->request->allowMethod(['patch', 'post', 'put']);
+        $this->loadModel('Search.SavedSearches');
+        $search = $this->SavedSearches->get($id);
+        $search = $this->SavedSearches->patchEntity($search, $this->request->data);
+        if ($this->SavedSearches->save($search)) {
+            $this->Flash->success(__('The search has been saved.'));
+        } else {
+            $this->Flash->error(__('The search could not be saved. Please, try again.'));
+        }
+        return $this->redirect(['action' => 'advanced', $search->model]);
+    }
+
+    /**
      * Search action
      *
      * @param  string $model model name
@@ -49,6 +70,10 @@ class SearchController extends AppController
             $where = $this->Searchable->prepareWhereStatement($this->request->data, $modelName, $advanced);
             $table = TableRegistry::get($modelName);
             $query = $table->find('all')->where($where);
+
+            /*
+            if in advanced mode, pre-save search criteria and results
+             */
             if ($advanced) {
                 $this->_preSaveSearchCriteriaAndResults($model, $query);
             }
