7d316436be0913919187bd8c51646b865bee2251
George Constantinou <g.constantinou@qobo.biz>
2016-05-11 12:26:44 +0300

    define dashboard columns through Configuration (task #1941)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
+use Cake\Core\Configure;
 use Cake\Event\EventManager;
 use Search\Events\SearchViewMenuListener;
+
+/*
+dashboards columns
+ */
+Configure::write('Search.dashboard.columns', ['Left Side', 'Right Side']);
 
 EventManager::instance()->on(new SearchViewMenuListener());
