b3e81a3fe6047d138d4955c30228cc853f6e0fa5
George Constantinou <g.constantinou@qobo.biz>
2016-06-21 18:09:50 +0300

    move part of SearchableComponent logic into a Trait (task #2207)

---
+++ src/Controller/Traits/SearchableTrait.php
@@ -0,0 +1,32 @@
+<?php
+namespace Search\Controller\Traits;
+
+use Cake\ORM\TableRegistry;
+
+trait SearchableTrait {
+    /**
+     * Returns true if table is searchable, false otherwise.
+     *
+     * @param  \Cake\ORM\Table|string $table Table object or name.
+     * @return bool
+     */
+    public function isSearchable($table)
+    {
+        $result = false;
+        /*
+        get Table instance
+         */
+        if (is_string($table)) {
+            $table = TableRegistry::get($table);
+        }
+
+        /*
+        check if is searchable
+         */
+        if (method_exists($table, 'isSearchable') && is_callable([$table, 'isSearchable'])) {
+            $result = $table->isSearchable();
+        }
+
+        return $result;
+    }
+}
