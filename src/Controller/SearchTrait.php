d846a42e0ce6cc027f7498ae731c2e32cf17ef66
George Constantinou <g.constantinou@qobo.biz>
2016-10-14 11:18:33 +0300

    fixes bug with display columns missing when saving search criteria or result right after a basic search (task #2766)

--- src/Controller/SearchTrait.php
+++ src/Controller/SearchTrait.php
@@ -105,6 +105,10 @@ trait SearchTrait
                     $this->request->data['criteria'],
                     $model
                 );
+            // set display columns before the pre-saving, fixes bug
+            // with missing display columns when saving a basic search
+            if (!$this->request->data('display_columns')) {
+                $this->request->data('display_columns', $table->getListingFields($model));
             }
             $search = $table->search($model, $this->Auth->user(), $this->request->data);
 
