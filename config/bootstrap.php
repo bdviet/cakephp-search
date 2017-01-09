320174cf0136272fd55a7d39f58ee2d995a9d477
George Constantinou <g.constantinou@qobo.biz>
2017-01-09 18:46:11 +0200

    remove obsolete Event listener (task #3151)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 use Cake\Core\Configure;
-use Cake\Event\EventManager;
-use Search\Event\View\SearchViewMenuListener;
 
 // dashboards columns
 Configure::write('Search.dashboard.columns', ['Left Side', 'Right Side']);
-
-EventManager::instance()->on(new SearchViewMenuListener());
